0a9c78c5671a9a4218b6e6e0ecfe50387ece0c90
abgdev1997 <adrianblancogarcia97@gmail.com>
2022-02-17 20:47:38 +0100

    security

--- src/main/java/com/example/taskscontactsbackendapp/repository/UserRepository.java
+++ src/main/java/com/example/taskscontactsbackendapp/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.taskscontactsbackendapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Long, User> {
+    Optional<Object> findByUsername(String username);
 }
---
+++ src/main/java/com/example/taskscontactsbackendapp/security/securityService/UserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.taskscontactsbackendapp.security.securityService;
+
+import com.example.taskscontactsbackendapp.models.User;
+import com.example.taskscontactsbackendapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Autentica un usuario de la base de datos
+ *
+ * Authentication Manager llama al método loadUserByUsername de esta clase
+ * para obtener los detalles del usuario de la base de datos cuando
+ * se intente autenticar un usuario
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user;
+        user = (User) userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),user.getPassword(),new ArrayList<>());
+    }
+}
