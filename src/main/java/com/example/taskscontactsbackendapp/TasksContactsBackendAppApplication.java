782a86656e9cb1c14ecd20e52592db906053764f
abgdev1997 <adrianblancogarcia97@gmail.com>
2022-02-17 20:13:55 +0100

    user model added

---
+++ src/main/java/com/example/taskscontactsbackendapp/TasksContactsBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskscontactsbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksContactsBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksContactsBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskscontactsbackendapp/TasksContactsBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskscontactsbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksContactsBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
